c1a7dcbe695dc03da3a97541946007c97680714c
saus-75 <georgechieng@gmail.com>
2018-03-05 01:19:31 +1100

    champion image added

---
+++ src/const.js
@@ -0,0 +1,5 @@
+export const region = {'Russia':'ru', 'Korea':'kr', 'Brazil':'br1', 
+                'Oceania':'oc1', 'Japan':'jp1', 'North America':'na1', 
+                'Europe Nordic & East':'eun1', 'Europe West':'euw1', 
+                'Turkey':'tr1', 'Latin America North':'la1', 
+                'Latin America South':'la2'};
